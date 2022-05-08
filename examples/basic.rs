39e40cf4c6d6e14b5c846d7b42e666ddeff2e707
pentamassiv <pentamassiv@posteo.de>
2022-05-08 22:56:23 +0200

    Not dropping the results

--- examples/basic.rs
+++ examples/basic.rs
@@ -11,18 +11,18 @@ fn main() {
     let vk_service = wayland_input::IMService::new::<DummyConnector>(None);
 
     // Enter a string
-    let submission_result = vk_service.commit_string("Start typing".to_string());
-    let submission_result = vk_service.commit();
-    if submission_result.is_err() {
-        println!("Error: {:?}", submission_result);
+    let submission_resulta = vk_service.commit_string("Start typing".to_string());
+    let submission_resultb = vk_service.commit();
+    if submission_resulta.is_err() && submission_resultb.is_err() {
+        println!("Error");
     };
     println!("Start typing");
 
     // Delete some text
-    let submission_result = vk_service.delete_surrounding_text(6, 0);
-    let submission_result = vk_service.commit();
-    if submission_result.is_err() {
-        println!("Error: {:?}", submission_result);
+    let submission_resulta = vk_service.delete_surrounding_text(6, 0);
+    let submission_resultb = vk_service.commit();
+    if submission_resulta.is_err() && submission_resultb.is_err() {
+        println!("Error");
     };
     println!("Deleted some letters");
 
@@ -32,12 +32,12 @@ fn main() {
     let release = wayland_input::KeyState::Released;
     let submission_result = vk_service.send_key(keycode, press);
     if submission_result.is_err() {
-        println!("Error: {:?}", submission_result);
+        println!("Error");
     };
     //let submission_result = vk_service.send_key(keycode, release);
     //let submission_result = vk_service.send_key(keycode, release);
     if submission_result.is_err() {
-        println!("Error: {:?}", submission_result);
+        println!("Error");
     };
     println!("Second toggle shift and long press x");
 
